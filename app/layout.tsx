17802db96425fcd80c619ba20a65fcf61ea06e3f
Tejas Nanoti <tejasnanoti2@gmail.com>
2024-05-23 21:37:07 +0530

    chore: Update metadata;

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tejas Nanoti | Web Dev | UI/UX | Machine Learning",
+  title: "Tejas Nanoti | Web Dev | Blockchain | Machine Learning",
   description: "Tejas Nanoti is a Full Stack Web developer based in India. Currently working on open-source projects and freelancing.",
   metadataBase: new URL('https://tejasnanoti.vercel.app'),
   openGraph: {
@@ -16,9 +16,9 @@ export const metadata: Metadata = {
     url: "https://tejasnanoti.vercel.app",
     images: {
       url: "/opengraph-image.png",
-      width: 1728,
-      height: 1117,
-      alt: "Tejas Nanoti | Web Dev | UI/UX | Machine Learning",
+      width: 1920,
+      height: 960,
+      alt: "Tejas Nanoti | Web Dev | Blockchain | Machine Learning",
     },
     siteName: "Tejas Nanoti's Personal Portfolio Website"
   }
